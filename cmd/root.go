f7f61e90fdd1d848adfc8e258a84fcea46198fe8
Rachit Srivastava <honey032004@gmail.com>
2025-05-13 00:53:41 +0530

    feat: initialized repo

---
+++ cmd/root.go
@@ -0,0 +1,43 @@
+/*
+Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "dots",
+	Short: "A Go-based CLI-Tool to manage your Dotfiles and sync with Git.",
+	Long: `Dots is a minimal, fast, and developer-friendly dotfile manager built in Go. 
+It helps you effortlessly manage, version, and sync your dotfiles using symlinks and Git, 
+without the complexity of bloated tools or manual setup..`,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	// Run: func(cmd *cobra.Command, args []string) { },
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.Dots.yaml)")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
