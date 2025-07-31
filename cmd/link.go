a484cb765bf8fb2bcdea4b03562a72dbc46b3421
Ethics03 <honey032004@gmail.com>
2025-07-31 18:02:34 +0530

    feat: link: links one file at a time, create cmd testing

--- cmd/link.go
+++ cmd/link.go
@@ -18,42 +18,44 @@ var linkCmd = &cobra.Command{
 	Short: "Create symlinks for tracked dotfiles.",
 	Long:  `Creates symbolic links from your dotfiles repo to their original paths.`,
 	Run: func(cmd *cobra.Command, args []string) {
+		
+		if len(args) != 1 {
+        fmt.Println("Usage: uriel link <dotfile>")
+        fmt.Println("Example: uriel link bashrc")
+        return
+    }
 
-		dotfiles := map[string]string{
-			"bashrc": ".bashrc",
-			"zshrc":  ".zshrc",
-		}
-
+		 name := args[0] // Only one file
+		
 		home, err := os.UserHomeDir()
 		if err != nil {
 			log.Fatal("Cannot find home directory", err)
 		}
 
 		dotsDir := filepath.Join(home, ".config", "dots")
+		src := filepath.Join(dotsDir, name)
+		desti := filepath.Join(home, name)
 
-		for name, target := range dotfiles {
-			src := filepath.Join(dotsDir, name)
-			desti := filepath.Join(home, target)
+			
+			if _, err := os.Stat(src); os.IsNotExist(err) {
+        fmt.Printf("Source file does not exist: %s\n", src)
+        return
+    }
 
-				if _, err := os.Stat(src); os.IsNotExist(err) {
-						fmt.Printf("Skipping: source file does not exist: %s\n", src)
-					continue
-				}
+     if _, err := os.Lstat(desti); err == nil {
+        fmt.Printf("Destination already exists: %s (skipping)\n", desti)
+        return
+    }
 
-				if _, err := os.Lstat(desti); err == nil {
-						fmt.Printf("Destination already exists: %s (skipping)\n", desti)
-						continue
-						}
+    err = os.Symlink(src, desti)
+    if err != nil {
+        fmt.Printf("Failed to link: %s -> %s: %v\n", src, desti, err)
+    } else {
+        fmt.Printf("Linked %s -> %s\n", src, desti)
+    }
 
-			err := os.Symlink(src, desti) //this is creating the symlink (./zshrc symlink pointing to zshrc in ./.config/.dots dir)
-			if err != nil {
-				fmt.Printf("Failed to link : %s -> %s: %v\n", src, desti, err)
-			} else {
-				fmt.Printf("Linked %s -> %s\n", src, desti) // this is printing the output
-			}
-		}
-	},
-}
+		},
+	}
 
 func init() {
 	rootCmd.AddCommand(linkCmd)
